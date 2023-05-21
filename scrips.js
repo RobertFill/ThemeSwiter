199fb55912973ba9561d134af7a738c3af6f7341
Filipe Robert <filipe_soares@yahoo.com>
2023-05-21 12:10:27 -0300

    ThemeSwitcher commit

--- scrips.js
+++ scrips.js
@@ -1,17 +1,14 @@
 let buttonSwitch = document.querySelector('.ball'); //class//
 let pageWhite = document.querySelector('body');
 let container = document.querySelector('.container');
-let bodyWhite = document.querySelector('html');
 
-buttonSwitch.onclick = e =>{
-    if(!container.classList.contains('light')){
+buttonSwitch.onclick = e => { //arrow function
+
+    if (!container.classList.contains('light')) { 
         container.classList.add('light');
-    }else{
+        document.body.style.background = '#292C35'
+    } else {
         container.classList.remove('light');
-    }
-    if(!bodyWhite.classList.contains('light')){
-        bodyWhite.classList.add('light');
-    }else{
-        bodyWhite.classList.remove('light');
+        document.body.style.background = '#f39c12'
     }
 }
