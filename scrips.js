2bc1ae94b6e1e5149057b5d9ebae9dbc14c41b74
Filipe Robert <filipe_soares@yahoo.com>
2023-07-03 12:51:35 -0300

    commit ThemeSwiter

--- scrips.js
+++ scrips.js
@@ -7,6 +7,7 @@ buttonSwitch.onclick = e => { //arrow function
     if (!container.classList.contains('light')) { 
         container.classList.add('light');
         document.body.style.background = '#292C35'
+        
     } else {
         container.classList.remove('light');
         document.body.style.background = '#f39c12'
